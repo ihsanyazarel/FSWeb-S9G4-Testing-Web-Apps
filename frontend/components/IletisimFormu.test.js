9d663aa3fdf24c873727007334b5257fd2785ff3
İhsan Yazarel <ihsanyazarel@hotmail.com>
2023-05-18 22:16:51 +0300

    Updated

--- frontend/components/IletisimFormu.test.js
+++ frontend/components/IletisimFormu.test.js
@@ -4,113 +4,92 @@ import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
 import IletisimFormu from './IletisimFormu';
 
+const getNameInput = () => { 
+    return screen.getByLabelText("Ad*"); //Ad inputu seçimi
+}
+const setNameInput = (val) =>{
+    userEvent.type(getNameInput(), val); //Ad inputuna veri girişi
+}
+const getSurnameInput = () => {
+    return screen.getByLabelText("Soyad*"); //Soyad inputu seçimi
+}
+const setSurnameInput = (val) =>{
+    userEvent.type(getSurnameInput(), val); //Soyad inputuna veri girişi
+}
+const getEmailInput = () => {
+    return screen.getByLabelText("Email*"); //Email inputu seçimi
+}
+const setEmailInput = (val) =>{
+    userEvent.type(getEmailInput(), val);  //Email inputuna veri girişi
+}
+const buttonClick = () => {
+    userEvent.click(screen.getByRole("button")); //Buton seçim ve click
+}
+const getError = () => {
+    return screen.queryByTestId("error");   //error id'li elementin seçimi
+}
+const getAllErrors = () => {
+    return screen.queryAllByTestId("error"); // error id'li tüm elementlerin seçimi. Array döndürür
+}
+
 beforeEach(() => {
     render(<IletisimFormu/>);
   });
 
 test('hata olmadan render ediliyor', async() => {
-    // render(<IletisimFormu/>);
+    //beforeEach
 });
 
 test('iletişim formu headerı render ediliyor', async() => {
-    // render(<IletisimFormu />);
-    const headerComponent = screen.getByText('İletişim Formu');
-    expect(headerComponent).toBeInTheDocument();
+    expect(screen.getByText('İletişim Formu')).toBeInTheDocument();
 });
 
 test('kullanıcı adını 5 karakterden az girdiğinde BİR hata mesajı render ediyor.', async () => {
-    const nameComponent = screen.getByLabelText("Ad*");
-    userEvent.type(nameComponent, "test"); 
-
-    const errors = screen.getAllByTestId("error");
-    expect(errors.length).toEqual(1);
+    setNameInput("test")
+    expect(getAllErrors().length).toEqual(1);
 });
 
 test('kullanıcı inputları doldurmadığında ÜÇ hata mesajı render ediliyor.', async () => {
-    const buttonComponent = screen.getByRole("button");
-    userEvent.click(buttonComponent);
-    const errors = screen.getAllByTestId("error");
-    expect(errors.length).toEqual(3);
-
+    buttonClick();
+    expect(getAllErrors().length).toEqual(3);
 });
 
 test('kullanıcı doğru ad ve soyad girdiğinde ama email girmediğinde BİR hata mesajı render ediliyor.', async () => {
-    const nameComponent = screen.getByLabelText("Ad*");
-    userEvent.type(nameComponent, "test5"); 
-    const surnameComponent = screen.getByLabelText("Soyad*");
-    userEvent.type(surnameComponent, "surname"); 
-    const buttonComponent = screen.getByRole("button");
-    userEvent.click(buttonComponent);
-
-    const errors = screen.getAllByTestId("error");
-    expect(errors.length).toEqual(1);
-
+    setNameInput("İhsan")
+    setSurnameInput("Yazarel")
+    buttonClick();
+    expect(getAllErrors().length).toEqual(1);
 });
 
 test('geçersiz bir mail girildiğinde "email geçerli bir email adresi olmalıdır." hata mesajı render ediliyor', async () => {
-    const mailComponent = screen.getByLabelText("Email*");
-    // expect(mailComponent).toBeInTheDocument();
-    userEvent.type(mailComponent, "test@mailcom");
-
-    const errors = screen.getByTestId("error");
-    expect(errors).toHaveTextContent("email geçerli bir email adresi olmalıdır.");
-    // expect(screen.getByText("Hata: email geçerli bir email adresi olmalıdır.")).toBeInTheDocument();
+    setEmailInput("wrong@mail");
+    expect(getError()).toHaveTextContent("email geçerli bir email adresi olmalıdır.");
 });
 
 test('soyad girilmeden gönderilirse "soyad gereklidir." mesajı render ediliyor', async () => {
-    const nameComponent = screen.getByLabelText("Ad*");
-    userEvent.type(nameComponent, "test5"); 
-    const surnameComponent = screen.getByLabelText("Soyad*");
-    const mailComponent = screen.getByLabelText("Email*");
-    userEvent.type(mailComponent, "test@mail.com");
-
-    const buttonComponent = screen.getByRole("button");
-    userEvent.click(buttonComponent);
-
-    const errors = screen.getByTestId("error");
-    // expect(errors.length).toEqual(1);
-    expect(errors).toHaveTextContent("soyad gereklidir.")
-
+    setNameInput("İhsan")
+    getSurnameInput();
+    setEmailInput("test@mail.com");
+    buttonClick();
+    expect(getError()).toHaveTextContent("soyad gereklidir.")
 });
 
 test('ad,soyad, email render ediliyor. mesaj bölümü doldurulmadığında hata mesajı render edilmiyor.', async () => {
-    const nameComponent = screen.getByLabelText("Ad*");
-    userEvent.type(nameComponent, "test5"); 
-    const surnameComponent = screen.getByLabelText("Soyad*");
-    userEvent.type(surnameComponent, "Surname"); 
-    const mailComponent = screen.getByLabelText("Email*");
-    userEvent.type(mailComponent, "test@mail.com");
-
-    const buttonComponent = screen.getByRole("button");
-    userEvent.click(buttonComponent);
-
-    expect(screen.queryByTestId("error")).toBe(null);
-
-
-
+    setNameInput("İhsan");
+    setSurnameInput("Yazarel");
+    setEmailInput("test@mail.com");
+    buttonClick();
+    expect(getError()).toBe(null);
 });
 
 test('form gönderildiğinde girilen tüm değerler render ediliyor.', async () => {
-    const nameComponent = screen.getByLabelText("Ad*");
-    userEvent.type(nameComponent, "test5"); 
-    const surnameComponent = screen.getByLabelText("Soyad*");
-    userEvent.type(surnameComponent, "Surname"); 
-    const mailComponent = screen.getByLabelText("Email*");
-    userEvent.type(mailComponent, "test@mail.com");
-    const mesajComponent = screen.getByLabelText("Mesaj");
-    userEvent.type(mesajComponent, "Mesaj"); 
-
-    const buttonComponent = screen.getByRole("button");
-    userEvent.click(buttonComponent);
-
-    const firstnameComponent = screen.getByTestId("firstnameDisplay");
-    expect(firstnameComponent).toHaveTextContent("test5");
-    const lastnameComponent = screen.getByTestId("lastnameDisplay");
-    expect(lastnameComponent).toHaveTextContent("Surname");
-    const emailComponent = screen.getByTestId("emailDisplay");
-    expect(emailComponent).toHaveTextContent("test@mail.com");
-    const messageComponent = screen.getByTestId("messageDisplay");
-    expect(messageComponent).toHaveTextContent("Mesaj");
-
-
+    setNameInput("İhsan");
+    setSurnameInput("Yazarel");
+    setEmailInput("test@mail.com");
+    userEvent.type(screen.getByLabelText("Mesaj"), "Herhangi bir mesaj"); 
+    buttonClick();
+    expect(screen.getByTestId("firstnameDisplay")).toHaveTextContent("İhsan");
+    expect(screen.getByTestId("lastnameDisplay")).toHaveTextContent("Yazarel");
+    expect(screen.getByTestId("emailDisplay")).toHaveTextContent("test@mail.com");
+    expect(screen.getByTestId("messageDisplay")).toHaveTextContent("Mesaj");
 });
